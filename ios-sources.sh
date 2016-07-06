7281c0bb68fb84b57da79539c6c177ec6198a7c5
Matt Schmidt <matt@digitaleye.com>
2016-07-06 10:39:27 -0400

    Move bigint outside of zxing dir

--- ios-sources.sh
+++ ios-sources.sh
@@ -12,12 +12,12 @@ SRCPATH='./src/ios/zxing-cpp/core/src'
 for f in $(find $SRCPATH -name '*.cc'); do
     filepath=${f[3,${#f}]}
     targetdir="$(print $f(:h:s!$SRCPATH!!))"
-    print "<source-file src=\"${filepath}\" target-dir=\"zxing/${targetdir[2,${#targetdir}]}\" />"
+    print "<source-file src=\"${filepath}\" target-dir=\"${targetdir[2,${#targetdir}]}\" />"
 done;
 for f in $(find $SRCPATH -name '*.hh'); do
     filepath=${f[3,${#f}]}
     targetdir="$(print $f(:h:s!$SRCPATH!!))"
-    print "<header-file src=\"${filepath}\" target-dir=\"zxing/${targetdir[2,${#targetdir}]}\" />"
+    print "<header-file src=\"${filepath}\" target-dir=\"${targetdir[2,${#targetdir}]}\" />"
 done;
 
 #now the main sources, excluding the win32 stuff
